cd47acdc23809f01a1e5d4e07143481de899e3dd
fha96 <fahadalrimawe96@hotmail.com>
2022-10-11 00:34:10 +0300

    complete example01 challenge02

--- javascript/code-challenges/tree-challenges/challenge02/challenge02.js
+++ javascript/code-challenges/tree-challenges/challenge02/challenge02.js
@@ -8,24 +8,47 @@ class Node {
     }
 }
 
-const p = new Node(1);
-const pTwo = new Node(2);
-const pThree = new Node(3);
-p.left = pTwo
-p.right = pThree
 
-const q = new Node(1);
-const qTwo = new Node(2);
-const qThree = new Node(3);
-q.left = qTwo;
-q.right = qThree;
-
-console.log('Q>>>>>>>>>>>>>',q);
-console.log('P>>>>>>>>>>>>>',p);
 
  function isSame(tree1, tree2) {
-    if(tree1.left && tree2.left && tree1.right && tree2.right){
-        
-    }    
+    let queue = [] ;
+    let queue2 = [] ;
+    queue.push(tree1);
+    queue2.push(tree2);
 
+    while(queue.length !== 0 && queue2.length !==0){
+        console.log('queue 1', queue);
+        console.log('queue 2', queue2);
+        let popped = queue.pop();
+        console.log('popppped value :', popped);
+        let popped2 = queue2.pop();
+        console.log('popppped222 value :', popped2);
+        if(popped.value !== popped2.value){
+           return false;
+        }
+        if(popped.left){
+            console.log(1);
+            queue.push(popped.left);
+        }
+        if(popped2.left){
+            console.log(2);
+            queue2.push(popped2.left);
+        }
+        if(popped.right){
+            console.log(1);
+            queue.push(popped.right);
+        }
+        if(popped2.right){
+            console.log(2);
+            queue2.push(popped2.right);
+        }
+
+    }
+    return true;
 }
+
+
+module.exports = {
+    Node,
+    isSame
+};
--- javascript/code-challenges/tree-challenges/challenge02/challenge02.test.js
+++ javascript/code-challenges/tree-challenges/challenge02/challenge02.test.js
@@ -1,1 +1,33 @@
 // Write your test here
+
+const {Node, isSame} = require("./challenge02");
+
+let p = null;
+let pTwo = null;
+let pThree = null;
+let q = null;
+let qTwo = null;
+let qThree = null;
+
+describe("test challenge 02", () => {
+    beforeAll(() => {
+       p = new Node(1);
+       pTwo = new Node(2);
+       pThree = new Node(3);
+      p.left = pTwo;
+      p.right = pThree;
+      q = new Node(1);
+      qTwo = new Node(2);
+      qThree = new Node(3);
+      q.left = qTwo;
+      q.right = qThree;
+
+    });
+  it("test example 1", () => {
+    
+    const checker = isSame(p,q);
+    expect(checker).toBeTruthy();
+  });
+
+  
+});
