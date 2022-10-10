137338dfc7c9c4f1fcae0ce0684a466a2e98e800
fha96 <fahadalrimawe96@hotmail.com>
2022-10-11 01:46:55 +0300

    complete challenge 02

--- javascript/code-challenges/tree-challenges/challenge02/challenge02.js
+++ javascript/code-challenges/tree-challenges/challenge02/challenge02.js
@@ -16,33 +16,39 @@ class Node {
     queue.push(tree1);
     queue2.push(tree2);
 
-    while(queue.length !== 0 && queue2.length !==0){
-        console.log('queue 1', queue);
-        console.log('queue 2', queue2);
-        let popped = queue.pop();
-        console.log('popppped value :', popped);
-        let popped2 = queue2.pop();
-        console.log('popppped222 value :', popped2);
-        if(popped.value !== popped2.value){
-           return false;
-        }
-        if(popped.left){
-            console.log(1);
-            queue.push(popped.left);
-        }
-        if(popped2.left){
-            console.log(2);
-            queue2.push(popped2.left);
-        }
-        if(popped.right){
-            console.log(1);
-            queue.push(popped.right);
-        }
-        if(popped2.right){
-            console.log(2);
-            queue2.push(popped2.right);
-        }
+    if(tree1.left && tree2.left){
 
+
+        while(queue.length !== 0 && queue2.length !==0){
+            console.log('queue 1', queue);
+            console.log('queue 2', queue2);
+            let popped = queue.pop();
+            console.log('popppped value :', popped);
+            let popped2 = queue2.pop();
+            console.log('popppped222 value :', popped2);
+            
+            if(popped.value !== popped2.value){
+                return false;
+            }
+            if(popped.left){
+                console.log(1);
+                queue.push(popped.left);
+            }
+            if(popped2.left){
+                console.log(2);
+                queue2.push(popped2.left);
+            }
+            if(popped.right){
+                console.log(1);
+                queue.push(popped.right);
+            }
+            if(popped2.right){
+                console.log(2);
+                queue2.push(popped2.right);
+            }
+        }
+    }else {
+        return false;
     }
     return true;
 }
--- javascript/code-challenges/tree-challenges/challenge02/challenge02.test.js
+++ javascript/code-challenges/tree-challenges/challenge02/challenge02.test.js
@@ -1,6 +1,6 @@
 // Write your test here
 
-const {Node, isSame} = require("./challenge02");
+const { Node, isSame } = require("./challenge02");
 
 let p = null;
 let pTwo = null;
@@ -8,26 +8,61 @@ let pThree = null;
 let q = null;
 let qTwo = null;
 let qThree = null;
+let p1 = null;
+let p2 = null;
+let p1Two = null;
+let p2Two = null;
+let t1 = null;
+let t2 = null;
+let t1One = null;
+let t2One = null;
+let t1TWo = null;
+let t2Two = null;
 
 describe("test challenge 02", () => {
-    beforeAll(() => {
-       p = new Node(1);
-       pTwo = new Node(2);
-       pThree = new Node(3);
-      p.left = pTwo;
-      p.right = pThree;
-      q = new Node(1);
-      qTwo = new Node(2);
-      qThree = new Node(3);
-      q.left = qTwo;
-      q.right = qThree;
+  beforeAll(() => {
+    p = new Node(1);
+    pTwo = new Node(2);
+    pThree = new Node(3);
+    p.left = pTwo;
+    p.right = pThree;
+    q = new Node(1);
+    qTwo = new Node(2);
+    qThree = new Node(3);
+    q.left = qTwo;
+    q.right = qThree;
 
-    });
+    p1 = new Node(1);
+    p2 = new Node(1);
+    p1Two = new Node(2);
+    p2Two = new Node(2);
+    p1.left = p1Two;
+    p2.right = p2Two;
+
+    t1 =new Node(1);
+    t1TWo =new Node(2);
+    t1One =new Node(1);
+    t2 =new Node(1);
+    t2One =new Node(1);
+    t2Two =new Node(2);
+
+    t1.left = t1TWo;
+    t1.right = t1One;
+
+    t2.left = t2One;
+    t2.right = t2Two;
+
+  });
   it("test example 1", () => {
-    
-    const checker = isSame(p,q);
+    const checker = isSame(p, q);
     expect(checker).toBeTruthy();
   });
-
-  
+  it("test example 2", () => {
+    const checker = isSame(p1, p2);
+    expect(checker).toEqual(false);
+  });
+  it("test example 3", () => {
+    const checker = isSame(t1, t2);
+    expect(checker).toEqual(false);
+  });
 });
