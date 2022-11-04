2b94e46aa30e9c8f221fed96de2def0ab44a8265
fha96 <fahadalrimawe96@hotmail.com>
2022-11-04 22:42:00 +0200

    initial commit

---
+++ javascript/code-challenges/hashtable-challenges/challenge01/challenge01.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+// by creating and check the copmliment of each element we are going to test arr,sum function
+
+
+const array = [7,2,9,1,5,null,14]; 
+const k = 3;
+
+function isSumEqual(arr, value) {
+    const compliments = {};
+    for(let i = 0 ; i <arr.length ; i++ ) {
+        if(arr[i] <= value){
+            let compHolder = value - arr[i];
+            compliments[compHolder] = arr[i];
+        }    
+            if(compliments[arr[i+1]]){
+                return true;
+            }
+    }
+    return false;
+}
+
+
+console.log(isSumEqual(array, k));
