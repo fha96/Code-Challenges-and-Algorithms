b3567596f89329b0cde8c4ec76338767425c4e16
fha96 <fahadalrimawe96@hotmail.com>
2022-11-05 13:16:12 +0200

    feat(challenge01) : add another solution

--- javascript/code-challenges/hashtable-challenges/challenge01/challenge01.js
+++ javascript/code-challenges/hashtable-challenges/challenge01/challenge01.js
@@ -23,3 +23,22 @@ function isSumEqual(arr, value) {
 
 
 console.log(isSumEqual(array, k));
+
+
+// function isSumEqual2(arr, value) {
+//     const compliments = {};
+//     for(let i = 0 ; i <arr.length ; i++ ) {
+//         if(arr[i] <= value){
+//             let compHolder = value - arr[i];
+//             compliments[compHolder] = arr[i];
+//         }
+//         let keys = Object.keys(compliments);    
+//             if(compliments.hasOwnProperty(arr[i+1])){
+//                 return true;
+//             }
+//     }
+//     return false;
+// }
+
+
+// console.log(isSumEqual2(array, k));
